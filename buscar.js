1112368695182afba387f21afbc2aa6bfeeeefc9
Tom-com-ar <tomasiezzi16@gmail.com>
2024-07-08 17:13:27 -0300

    barra de busqueda y fondo img

--- buscar.js
+++ buscar.js
@@ -13,5 +13,21 @@ document.addEventListener("DOMContentLoaded", function() {
                 container.style.display = 'none';
             }
         });
+
+    });
+
+    searchBar.addEventListener('keyup', function() {
+        const searchTerm = searchBar.value.toLowerCase();
+        const containers = document.querySelectorAll('.container1');
+
+        containers.forEach(container1 => {
+            const title = container1.querySelector('h3').textContent.toLowerCase();
+            if (title.includes(searchTerm)) {
+                container1.style.display = '';
+            } else {
+                container1.style.display = 'none';
+            }
+        });
+
     });
 })
