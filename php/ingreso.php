74cf7b5452570dfdf2f87122abf4cfa75db0b143
Tom-com-ar <tomasiezzi16@gmail.com>
2024-09-10 16:32:09 -0300

    Comentarios y Alertas

--- php/ingreso.php
+++ php/ingreso.php
@@ -1,32 +1,32 @@
-<?
+<?php
 
 // Establecer la conexión con la base de datos
-$conexion = new mysqli('localhost', 'phpmyadmin', 'RedesInformaticas','tiezzi');
+$conexion = new mysqli('localhost', 'phpmyadmin', 'RedesInformaticas', 'tiezzi');
 
 // Verificar si la conexión fue exitosa
-if (!$conexion) {
-    die('Error de conexión:');
+if ($conexion->connect_error) {
+    die('Error de conexión: ' . $conexion->connect_error);
 }
 
 // Recoger los datos enviados por POST y asegurar que estén escapados
-$nombre = $_POST['NAMEPHP'];
-$apellido = $_POST['APELLIDOPHP'];
-$telefono = $_POST['TELEFONOPHP'];
-$email = $_POST['EMAILPHP'];
-$mensaje = $_POST['mensaje'];
+$nombre = mysqli_real_escape_string($conexion, $_POST['NAMEPHP']);
+$apellido = mysqli_real_escape_string($conexion, $_POST['APELLIDOPHP']);
+$telefono = mysqli_real_escape_string($conexion, $_POST['TELEFONOPHP']);
+$email = mysqli_real_escape_string($conexion, $_POST['EMAILPHP']);
+$mensaje = mysqli_real_escape_string($conexion, $_POST['mensaje']);
 
 // Crear la consulta SQL para insertar los datos en la tabla 'comentarios'
-$sql = "INSERT INTO 'comentarios' ('Nombre', 'Apellidos', 'Teléfono', 'Email', 'Mensaje') 
+$sql = "INSERT INTO `comentarios` (`Nombre`, `Apellidos`, `Teléfono`, `Email`, `Mensaje`) 
         VALUES ('$nombre', '$apellido', '$telefono', '$email', '$mensaje')";
 
-
-if (mysqli_query($sql) === TRUE) {
-    echo "El ingreso ha sido satisfactorio. Gracias por tu comentario.";
+// Ejecutar la consulta y verificar si fue exitosa
+if ($conexion->query($sql) === TRUE) {
+    echo "Gracias por mandarnos un comentario. Ingreso exitoso.";
 } else {
-    echo "Error al ingresar datos: " . error;
+    echo '<script>alert("Error al ingresar datos: ' . $conexion->error;
 }
 
 // Cerrar la conexión
-mysqli_close($conexion);
+$conexion->close();
 
 ?>
